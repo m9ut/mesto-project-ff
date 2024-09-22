f2c458509647749da09c2b21590243414018a05e
Gumbert Doomer <arsyakov.alex@yandex.ru>
2024-09-22 19:13:40 +0300

    v1

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,24 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
-const cardsTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+const cardCreate = (name, link, removeCard) => {
+  const cardElement = cardTemplate.cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  cardElement.querySelector(".card__image").src = link;
+  cardElement.querySelector(".card__image").alt = name;
+  cardElement.querySelector(".card__title").textContent = name;
+  deleteButton.addEventListener("click", function (event) {
+    cardRemove(event);
+  });
+  return cardElement;
+};
 
-const placesList = document.querySelector('.places__list');
-
-// @todo: Функция создания карточки
-
-const createCard = (name, link, removeCard) => {
-    const placesItem = cardsTemplate.cloneNode(true);
-    const deleteButton = placesItem.querySelector('.card__delete-button');
-    placesItem.querySelector('.card__image').src = link;
-    placesItem.querySelector('.card__image').alt = name;
-    placesItem.querySelector('.card__title').textContent = name;
-    deleteButton.addEventListener('click', (event) => {
-      removeCard(event);
-    });
-
-    return placesItem;
-}
-
-// @todo: Функция удаления карточки
-
-const removeCard = (event) => {
-    let placeToDelete = event.target.closest('.places__item');
-    placeToDelete.remove();
-  }
-
-// @todo: Вывести карточки на страницу
+const cardRemove = function (event) {
+  let cardDelete = event.target.closest(".places__item");
+  cardDelete.remove();
+};
 
 initialCards.forEach((item) => {
-    addCard(item.name, item.link, removeCard);
-    placesList.append(createCard(item.name, item.link, removeCard));
-  })
+  placesList.append(cardCreate(item.name, item.link, cardRemove));
+});
