a97276aa78d5d096696fc9cbafd7f34c764669e2
Gumbert Doomer <arsyakov.alex@yandex.ru>
2024-12-18 21:09:40 +0300

    fix proj6

--- src/components/card.js
+++ src/components/card.js
@@ -1,33 +1,33 @@
-const cardsTemplate = document.querySelector('#card-template').content;
+const cardsTemplate = document.querySelector("#card-template").content;
 
-function createCard  (element, deleteCard, getLike, openImg) {
-  const cardItem = cardsTemplate.querySelector('.card').cloneNode(true);
-  const cardImage = cardItem.querySelector('.card__image');
-  const cardContent = cardItem.querySelector('.card__title');
-  const cardLikeButton = cardItem.querySelector('.card__like-button');
-  const cardDeleteButton = cardItem.querySelector('.card__delete-button');
+function createCard(element, deleteCard, getLike, openImg) {
+  const cardItem = cardsTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardItem.querySelector(".card__image");
+  const cardContent = cardItem.querySelector(".card__title");
+  const cardLikeButton = cardItem.querySelector(".card__like-button");
+  const cardDeleteButton = cardItem.querySelector(".card__delete-button");
 
   cardImage.src = element.link;
   cardImage.alt = element.name;
   cardContent.textContent = element.name;
 
-  cardDeleteButton.addEventListener('click', ()=> {
+  cardDeleteButton.addEventListener("click", () => {
     deleteCard(cardItem);
   });
 
-  cardLikeButton.addEventListener('click', getLike);
+  cardLikeButton.addEventListener("click", getLike);
 
-  cardImage.addEventListener('click', openImg);
+  cardImage.addEventListener("click", openImg);
 
   return cardItem;
 }
 
-function deleteCard (element) {
+function deleteCard(element) {
   element.remove();
 }
 
-function getLike (event) {
-  event.target.classList.toggle('card__like-button_is-active');
+function getLike(event) {
+  event.target.classList.toggle("card__like-button_is-active");
 }
 
-export { createCard , deleteCard, getLike };
+export { createCard, deleteCard, getLike };
--- src/index.js
+++ src/index.js
@@ -1,61 +1,77 @@
-import './pages/index.css';
+import "./pages/index.css";
 
-import { initialCards } from './components/cards.js';
+import { initialCards } from "./components/cards.js";
 
-import { createCard, deleteCard, getLike } from './components/card.js';
+import { createCard, deleteCard, getLike } from "./components/card.js";
 
-import { profileEditButton, profilePopup, profileForm, profileName, profileJob, profileNameContent, profileJobContent, addCardButton, cardPopup, cardForm, cardName, cardLink, imageItem, imagePopup, imageSign } from './components/constants.js';
+import {
+  profileEditButton,
+  profilePopup,
+  profileForm,
+  profileName,
+  profileJob,
+  profileNameContent,
+  profileJobContent,
+  addCardButton,
+  cardPopup,
+  cardForm,
+  cardName,
+  cardLink,
+  imageItem,
+  imagePopup,
+  imageSign,
+} from "./components/constants.js";
 
-import { openModal, handleClick, closeModal } from './components/modal.js'
+import { openModal, handleClick, closeModal } from "./components/modal.js";
 
-const cardsContainer = document.querySelector(".places__list"); 
+const cardsContainer = document.querySelector(".places__list");
 
-function handleProfileFormSubmit (event) {
-  event.preventDefault()
+function handleProfileFormSubmit(event) {
+  event.preventDefault();
   profileNameContent.textContent = profileName.value;
   profileJobContent.textContent = profileJob.value;
   closeModal(profilePopup);
 }
 
-profileEditButton.addEventListener('click', () => {
+profileEditButton.addEventListener("click", () => {
   profileName.value = profileNameContent.textContent;
   profileJob.value = profileJobContent.textContent;
   openModal(profilePopup);
-})
+});
 
-profilePopup.addEventListener('click', handleClick);
-profileForm.addEventListener('submit',  handleProfileFormSubmit);
+profilePopup.addEventListener("click", handleClick);
+profileForm.addEventListener("submit", handleProfileFormSubmit);
 
-function addCard (element) {
+function addCard(element) {
   cardsContainer.append(createCard(element, deleteCard, getLike, openImg));
 }
 
 initialCards.forEach((element) => addCard(element));
 
-addCardButton.addEventListener('click', ()=> {
+addCardButton.addEventListener("click", () => {
   openModal(cardPopup);
 });
 
-cardPopup.addEventListener('click', handleClick);
+cardPopup.addEventListener("click", handleClick);
 
-function getCard (event) {
+function getCard(event) {
   event.preventDefault();
   const cardInfo = {
     name: cardName.value,
-    link: cardLink.value
-  } 
+    link: cardLink.value,
+  };
   cardsContainer.prepend(createCard(cardInfo, deleteCard, getLike, openImg));
   cardForm.reset();
   closeModal(cardPopup);
 }
 
-cardForm.addEventListener('submit', getCard);
+cardForm.addEventListener("submit", getCard);
 
-function openImg (event) {
+function openImg(event) {
   imageItem.src = event.target.src;
   imageItem.alt = event.target.alt;
   imageSign.textContent = event.target.alt;
   openModal(imagePopup);
 }
 
-imagePopup.addEventListener('click', handleClick);
+imagePopup.addEventListener("click", handleClick);
